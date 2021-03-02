9a3334c14ab9bcc84a2c0e8fd03ef45d7308a7ab
SaurabhSonde <saurabhsonde111@gmail.com>
2021-03-02 19:20:10 +0530

    firebase configuration done

---
+++ src/components/UploadForm.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const UploadForm = () => {
+  const handleChange = (e) => {
+    //
+  };
+
+  return (
+    <form>
+      <input type="file" onChange={handleChange} />
+    </form>
+  );
+};
+
+export default UploadForm;
