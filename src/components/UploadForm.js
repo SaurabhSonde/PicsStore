0c49eb81c95b60eed9b36de97660b9ad779d70ba
SaurabhSonde <saurabhsonde111@gmail.com>
2021-03-03 18:45:54 +0530

    handled the change and set all errors and file

--- src/components/UploadForm.js
+++ src/components/UploadForm.js
@@ -1,13 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 
 const UploadForm = () => {
+  const [file, setFile] = useState(null);
+  const [error, setError] = useState(null);
+
+  const imageTypes = ["image/png", "image/jpeg"];
+
   const handleChange = (e) => {
-    //
+    let selected = e.target.files[0];
+    if (selected && imageTypes.includes(selected.type)) {
+      setFile(selected);
+      setError("");
+    } else {
+      setFile(null);
+      setError("Please select an image file (png or jpeg)");
+    }
   };
 
   return (
     <form>
       <input type="file" onChange={handleChange} />
+      <span>+</span>
+      <div className="output">
+        {error && <div className="error">{error}</div>}
+        {file && <div>{file.name}</div>}
+      </div>
     </form>
   );
 };
