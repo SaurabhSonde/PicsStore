495847a5e905fc3534e773d065a3244803a90ac5
SaurabhSonde <saurabhsonde111@gmail.com>
2021-03-03 19:56:20 +0530

    fixed Ui issue

--- src/components/UploadForm.js
+++ src/components/UploadForm.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import ProgressBar from "./ProgressBar";
 
 const UploadForm = () => {
   const [file, setFile] = useState(null);
@@ -19,11 +20,14 @@ const UploadForm = () => {
 
   return (
     <form>
-      <input type="file" onChange={handleChange} />
-      <span>+</span>
+      <label>
+        <input type="file" onChange={handleChange} />
+        <span>+</span>
+      </label>
       <div className="output">
         {error && <div className="error">{error}</div>}
         {file && <div>{file.name}</div>}
+        {file && <ProgressBar file={file} setFile={setFile} />}
       </div>
     </form>
   );
