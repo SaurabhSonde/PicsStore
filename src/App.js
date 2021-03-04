6e9818ced0b19e1123a4c49b6a076760616af22f
SaurabhSonde <saurabhsonde111@gmail.com>
2021-03-04 14:50:29 +0530

    passing selected img to model

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import ImageGrid from "./components/ImageGrid";
 import Model from "./components/Model";
 import Title from "./components/Title";
 import UploadForm from "./components/UploadForm";
 
 function App() {
+  //to select the image for work of the model
+  const [selectedImg, setSelectedImg] = useState(null);
   return (
     <div className="App">
       <Title />
       <UploadForm />
-      <ImageGrid />
-      <Model />
+      <ImageGrid setSelectedImg={setSelectedImg} />
+      <Model selectedImg={selectedImg} />
     </div>
   );
 }
