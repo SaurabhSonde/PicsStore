c8421eb3c8ab45a5455c5a6760077e4d9325a65d
SaurabhSonde <saurabhsonde111@gmail.com>
2021-03-04 15:46:48 +0530

    description changed

--- src/components/Title.js
+++ src/components/Title.js
@@ -5,7 +5,7 @@ const Title = () => {
     <div className="title">
       <h1>PicsStore</h1>
       <h2>Your Pictures</h2>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      <p>All your beautiful pictures at one place.</p>
     </div>
   );
 };
